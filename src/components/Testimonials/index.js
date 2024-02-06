2e0b4780461b3e916130c661ac081ba0854fe7e1
Yaminireddy-27 <yaminireddysavva@gmail.com>
2024-02-06 22:22:11 +0530

    testimonials

---
+++ src/components/Testimonials/index.js
@@ -0,0 +1,61 @@
+import { Component } from "react";
+
+import { TopContainer } from "./styledComponents";
+import { StoriesContainer } from "./styledComponents";
+import { Heading } from "./styledComponents";
+import { ConsumerCon } from "./styledComponents";
+import { CR } from "./styledComponents";
+import { CrEle } from "./styledComponents";
+import { Desc } from "./styledComponents";
+import { BottomCon } from "./styledComponents";
+import { Button } from "./styledComponents";
+import { BottomHeading } from "./styledComponents";
+import { MeetingContainer } from "./styledComponents";
+import { Image } from "./styledComponents";
+import { BottomDesc } from "./styledComponents";
+
+class Testimonials extends Component {
+  render() {
+    return (
+      <div>
+        <TopContainer>
+          <StoriesContainer>
+            <Heading>Testimonials and Success Stories</Heading>
+            <ConsumerCon>
+              <CR>CR</CR>
+              <div>
+                <CrEle>Consumer</CrEle>
+                <CrEle>Reports</CrEle>
+              </div>
+            </ConsumerCon>
+            <Desc>
+              Hear from our clients about how Capital Numbers has transformed
+              their businesses
+            </Desc>
+          </StoriesContainer>
+          <div>
+            <p>Carousel</p>
+          </div>
+        </TopContainer>
+        <BottomCon>
+          <Image
+            src="https://res.cloudinary.com/dpezm2x4v/image/upload/v1707233784/Screenshot_2024-02-06_204758_dr6g89.png"
+            alt="meeting"
+          />
+          <MeetingContainer>
+            <BottomHeading>
+              The Brightest Minds in Digital At Your Service
+            </BottomHeading>
+            <BottomDesc>
+              Partnering across start-ups to SMBs to corporate businesses,we
+              deliver innovation that is measurable, mature, and meaningful.
+            </BottomDesc>
+            <Button>Schedule Meeting</Button>
+          </MeetingContainer>
+        </BottomCon>
+      </div>
+    );
+  }
+}
+
+export default Testimonials;
